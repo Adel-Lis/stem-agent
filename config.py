601da97579eb845f78820ada84fdbaba12598a13
Adel13Lis <adel.lis.work@gmail.com>
2026-04-29 18:33:26 +0200

    Define the config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+MODEL = "gpt-4o-mini"
+
+MAX_GROWTH_CYCLES = 10 # I want to prevent infinite loops
+MIN_GROWTH_CYCLES = 3
+
+BASE_OUTPUT_DIR = "outputs"
+
+MAX_SEARCH_RESULTS = 5 # I set a limit to how many the builder reads from the web query
